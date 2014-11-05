841b1f8fb55fda519356117f2711fc94a1874322
Chae <chaemobile@gmail.com>
2014-11-05 14:17:57 -0500

    Used Chase as middleware
    * without throwing away Lobster response

--- chaese.rb
+++ chaese.rb
@@ -23,12 +23,10 @@ COOL_MESSAGE
 
     status, headers, response = @app.call(env)
 
-    response = ["<title>The chaese stands alone.</title>",
-                "<pre>", NOT_QUITE_STRING_CHAESE, "</pre>"]
+    response.write "<title>The chaese stands alone.</title>"
+    response.write "<pre> #{NOT_QUITE_STRING_CHAESE} </pre>"
+    headers["Content-Length"] = response.length.to_s
 
-    length = response.inject(0) { |a,e| a+e.size }.to_s
-    headers["Content-Length"] = length
-
-    [200, headers, response]
+    [status, headers, response]
   end
 end
